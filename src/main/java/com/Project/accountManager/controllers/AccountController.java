fb1f366a9224b61f47639c284907cf4875ec1dea
Serhat Beştaş <serhatbestas9@gmail.com>
2024-05-23 15:40:34 +0300

    AccountController has been created. codes to add, update, list and delete accounts have been written.

---
+++ src/main/java/com/Project/accountManager/controllers/AccountController.java
@@ -0,0 +1,53 @@
+package com.Project.accountManager.controllers;
+
+import com.Project.accountManager.entities.Account;
+import com.Project.accountManager.request.AccountCreateRequest;
+import com.Project.accountManager.request.AccountUpdateRequest;
+import com.Project.accountManager.services.AccountService;
+import com.Project.accountManager.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    UserService userService;
+
+    public AccountController(UserService userService, AccountService accountService) {
+        this.userService = userService;
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public List<Account> getAllAccount(@RequestParam Optional<Long> userId){
+        return accountService.getAllAccount(userId);
+    }
+
+    @PostMapping
+    public Account createAccount(@RequestBody AccountCreateRequest newAccount){
+        return accountService.saveOneAccount(newAccount);
+    }
+    @GetMapping("/{accountId}")
+    public Account getOneAccount(@PathVariable Long accountId){
+        return accountService.getOneAccount(accountId);
+    }
+    @PutMapping("/{accountId}")
+    public Account updateAccount(@PathVariable Long accountId, @RequestBody AccountUpdateRequest accountUpdateRequest){
+        return accountService.updateOneAccount(accountId,accountUpdateRequest);
+    }
+
+    @DeleteMapping("/{accountId}")
+    public void deleteOneAccount(@PathVariable Long accountId){
+        accountService.deleteOneAccount(accountId);
+    }
+
+
+
+}
