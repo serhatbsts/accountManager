909ea2056c0ac47e19eb243b2c598a734d16e1b3
Serhat Beştaş <serhatbestas9@gmail.com>
2024-05-23 14:17:14 +0300

    Updated readme.md file, created user entities and account entities.

---
+++ src/main/java/com/Project/accountManager/entities/User.java
@@ -0,0 +1,24 @@
+package com.Project.accountManager.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="user")
+@Data
+public class User {
+    @Id
+    private Long id;
+    String name;
+    String surName;
+    int password;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
