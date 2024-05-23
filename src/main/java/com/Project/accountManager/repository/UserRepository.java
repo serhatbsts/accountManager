9f3f432c58fc266c7efd7e8db90b2664bf4539df
Serhat Beştaş <serhatbestas9@gmail.com>
2024-05-23 14:02:49 +0300

    will be edited.

--- src/main/java/com/Project/accountManager/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Project.accountManager.repository;
-
-import com.Project.accountManager.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
--- src/main/java/com/Project/accountManager/request/AccountUpdateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.Project.accountManager.request;
-
-import lombok.Data;
-
-@Data
-public class AccountUpdateRequest {
-
-}
