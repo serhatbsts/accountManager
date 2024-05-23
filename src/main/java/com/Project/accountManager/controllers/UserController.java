e9ea617ae90f000964f862c25fac195b0ef53ee5
Serhat Beştaş <serhatbestas9@gmail.com>
2024-05-23 15:31:04 +0300

    UserController has been created. codes to add, update, list and delete accounts have been written.

---
+++ src/main/java/com/Project/accountManager/controllers/UserController.java
@@ -0,0 +1,41 @@
+package com.Project.accountManager.controllers;
+
+import com.Project.accountManager.entities.User;
+import com.Project.accountManager.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUser();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User newUser){
+        return userService.saveOneUser(newUser);
+    }
+    @GetMapping("/{userId}")
+    public User getOneUser(@PathVariable Long userId){
+        return userService.getOneUser(userId);
+    }
+    @PutMapping("/{userId}")
+    public User updateOneUser(@PathVariable Long userId,@RequestBody User newUser){
+        return userService.updateOneUser(userId,newUser);
+    }
+    @DeleteMapping("/{userId}")
+    public void deleteOneUser(@PathVariable Long userId){
+        userService.deleteOneUser(userId);
+    }
+}
